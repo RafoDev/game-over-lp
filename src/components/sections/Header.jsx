f13dd8093e3540f8c37eda800402f563a8d6b698
mariela-rodriguez <mariela.rodriguez@pucp.pe>
2022-05-23 01:08:18 -0500

    Dark mode

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+// import darkMode from '../header/DarkMode';
+
 const Header = () => {
-    const navToggle = document.getElementById('header-nav__menu-icon-label-close');
     const handleToggle = (() => {
+        const navToggle = document.getElementById('header-nav__menu-icon-label-close');
         const menuLinkList = document.querySelector('.header-nav__menu-link-list');
         menuLinkList.classList.toggle('header-nav__menu-link-list-show');
         navToggle.classList.toggle('header-nav__menu-icon-label-close-show');
     });
+
+    const handleSwitchToggle = (() => {
+        const btnSwitch = document.getElementById('header-nav__menu-switch');
+        document.body.classList.toggle('body-dark');
+        btnSwitch.classList.toggle('active');
+
+        if (document.body.classList.contains('body-dark')) {
+            localStorage.setItem('dark-mode', 'true');
+        } else {
+            localStorage.setItem('dark-mode', 'false');
+        }
+    });
+
     return (
         <header className="header">
             <div className="header-container">
@@ -25,7 +40,7 @@ const Header = () => {
                     </div>
                     <ul className="header-nav__menu-link-list">
                         <li className="header-nav__list-item">
-                            <a href="" className="header-nav__link">Inicio</a>
+                            <a href="#" className="header-nav__link">Inicio</a>
                         </li>
                         <li className="header-nav__list-item">
                             <a href="" className="header-nav__link">Productos</a>
@@ -34,7 +49,9 @@ const Header = () => {
                             <a href="" className="header-nav__link">Contacto</a>
                         </li>
                         <li>
-                            <button className="header-nav__menu-switch" id="header-nav__menu-switch">
+                            <button className="header-nav__menu-switch" id="header-nav__menu-switch"
+                            onClick={handleSwitchToggle}
+                            >
                                 <span className="header-nav__menu-switch-label"><i className="bi bi-emoji-sunglasses"></i></span>
                                 <span className="header-nav__menu-switch-label"><i className="bi bi-moon-stars-fill"></i></span>
                             </button>
